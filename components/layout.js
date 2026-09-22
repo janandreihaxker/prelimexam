c68c94e2c7e884b017258a80854c013d66c6b4ba
janandreihaxker <janandrei5033@gmail.com>
2026-09-22 14:28:10 +0800

    Match blog2 layout structure

--- components/layout.js
+++ components/layout.js
@@ -6,17 +6,28 @@ import Link from 'next/link';
 
 const name = 'Jan Andrei Teresa';
 
-export const siteTitle = '10 Things That Require Zero Talent - Jan Andrei Teresa';
+export const siteTitle = 'Jan Andrei  - Personal Website';
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+
         <meta
           name="description"
-          content="Jan Andrei Teresa - 10 Things That Require Zero Talent"
+          content="Learn more about Jan Andrei and his personal website"
         />
+
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=${encodeURIComponent(
+            'https://nextjs.org/static/nextjs-logo.svg'
+          )}`}
+        />
+
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
@@ -30,8 +41,9 @@ export default function Layout({ children, home }) {
               className={utilStyles.borderCircle}
               height={144}
               width={144}
-              alt="Jan Andrei Teresa"
+              alt="Jan Andrei"
             />
+
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
@@ -43,9 +55,10 @@ export default function Layout({ children, home }) {
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
-                alt="Jan Andrei Teresa"
+                alt="Jan Andrei"
               />
             </Link>
+
             <h2 className={utilStyles.headingLg}>
               <Link href="/" className={utilStyles.colorInherit}>
                 {name}
